20ae3ae5837230429ef47588d6f5194347b0b6d0
chLi <565353780@qq.com>
2024-08-22 00:17:57 +0800

    simplify compile command

--- compile.sh
+++ compile.sh
@@ -1,11 +1,8 @@
-#rm -rf build
+rm -rf build
 
 mkdir build
 cd build
 cmake \
   -DCMAKE_PREFIX_PATH=$(python3 -c 'import torch;print(torch.utils.cmake_prefix_path)') \
-  -DOpen3D_ROOT=../../open3d-manage/3rd/open3d-devel-linux/ \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   ..
 make -j
