25e189f78cc02732d1554e9f839eb1a7d3dadc5e
chLi <565353780@qq.com>
2025-03-10 14:13:47 +0800

    simplify setup

--- dev_setup.sh
+++ dev_setup.sh
@@ -10,8 +10,6 @@ cd ../aro-net
 
 pip install -U torch torchvision torchaudio
 
-python setup.py build_ext --inplace
-
 mkdir ssl
 cd ssl
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes - batch
